adf533d91cceb319fabbe578dd3262a96d2175a8
AAAAAEXQOSyIpN2JZ0ehUQ <36234074+AAAAAEXQOSyIpN2JZ0ehUQ@users.noreply.github.com>
2022-10-26 01:41:22 -0300

    Update Script

--- Install/senharoot.sh
+++ Install/senharoot.sh
@@ -1,12 +1,15 @@
 #!/bin/bash
 # By crazy_vpn
 # Pequeno script para permissao de autenticacao root
+# Aplicar permissoes de usuario root aos sistemas
+# Oracle, Aws, Azure, Google, Amazon e etc
 clear
 [[ "$(whoami)" != "root" ]] && {
 	clear
-	echo -e "\033[1;31mEXECULTE COMO USUARIO ROOT, \033[1;32m(\033[1;33msudo -i\033[1;32m)\033[0m"
+	echo -e "\033[1;31mEJECUTE COMO USUARIO ROOT, \033[1;32m(\033[1;33msudo -i\033[1;32m)\033[0m"
 	exit
 }
+echo -e "\n\033[1;32mPERMISOS DE AUTENTIFICACION ROOT... \033[0m"
 [[ $(grep -c "prohibit-password" /etc/ssh/sshd_config) != '0' ]] && {
 	sed -i "s/prohibit-password/yes/g" /etc/ssh/sshd_config
 } > /dev/null
@@ -33,10 +36,13 @@ iptables -A INPUT -p tcp --dport 443 -j ACCEPT
 iptables -A INPUT -p tcp --dport 8799 -j ACCEPT
 iptables -A INPUT -p tcp --dport 8080 -j ACCEPT
 iptables -A INPUT -p tcp --dport 1194 -j ACCEPT
-clear && echo -ne "\033[1;32mDEFINA A SENHA ROOT\033[1;37m: "; read senha
+sleep 3
+echo -e "\n\033[1;31m[ \033[1;33mOK ! \033[1;31m]\033[1;37m - \033[1;32mPERMISOS APLICADOS ! \033[0m"
+echo ""
+echo -ne "\033[1;32mESCOJA UNA NUEVA CONTRASEÑA ROOT\033[1;37m: "; read senha
 [[ -z "$senha" ]] && {
-echo -e "\n\033[1;31mSENHA INVALIDA !\033[0m"
+echo -e "\n\033[1;31mCONTRASEÑA INVALIDA !\033[0m"
 exit 0
 }
 echo "root:$senha" | chpasswd
-echo -e "\n\033[1;31m[ \033[1;33mOK ! \033[1;31m]\033[1;37m - \033[1;32mSENHA DEFINIDA ! \033[0m"
+echo -e "\n\033[1;31m[ \033[1;33mOK ! \033[1;31m]\033[1;37m - \033[1;32mNUEVA CONTRASEÑA DEFINIDA ! \033[0m"
